b0b661095657be18b33542d9de2334abb896858f
benedictgodhana <113383009+benedictgodhana@users.noreply.github.com>
2022-11-12 13:26:10 +0300

    Add files via upload

---
+++ profit determiner.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//variable declaration
+int buyingprice, sellingprice,profit;
+
+//variable initialization
+cout<<"Enter Buying Price:";
+cin>>buyingprice;
+cout<<"Enter Selling Price:";
+cin>>sellingprice;
+
+profit=sellingprice-buyingprice;
+
+
+
+//output
+
+cout<<"Profit:"<<profit<<endl;
+
+
+
+
+}
